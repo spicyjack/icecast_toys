894f220408711117e3b8f0cbb6c6e97ec97b6786
Brian Manning <brian@xaoc.org>
2012-03-26 19:58:43 -0700

    added threaded XML parser from D72ThreadedXmlParser

--- ios/TestCode/IcecastStatus/IcecastStatus/AppDelegate.h
+++ ios/TestCode/IcecastStatus/IcecastStatus/AppDelegate.h
@@ -2,15 +2,14 @@
 //  AppDelegate.h
 //  IcecastStatus
 //
-//  Created by Brian Manning on 2/28/12.
-//  Copyright (c) 2012 Example Company Inc. All rights reserved.
+//  Created by Brian Manning on 3/1/12.
+//  Copyright (c) 2012 Example Company. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, 
-    NSURLConnectionDataDelegate,
-    NSXMLParserDelegate>
+@interface AppDelegate : UIResponder 
+                <UIApplicationDelegate, NSXMLParserDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
--- ios/TestCode/IcecastStatus/IcecastStatus/AppDelegate.m
+++ ios/TestCode/IcecastStatus/IcecastStatus/AppDelegate.m
@@ -2,66 +2,144 @@
 //  AppDelegate.m
 //  IcecastStatus
 //
-//  Created by Brian Manning on 2/28/12.
+//  Created by Brian Manning on 3/1/12.
 //  Copyright (c) 2012 Example Company Inc. All rights reserved.
 //
 
 #import "AppDelegate.h"
+#import "Video.h"
+
+// enumerated type
+enum ParserState { xmlInit, xmlTitle, xmlDescription };
 
 @implementation AppDelegate
-// implementation variables
 {
-    NSMutableString *ms;
-    UITextView *textView;
-    NSMutableData *rawXmlData;
-}
-
--(void) connection:(NSURLConnection *)connection 
-    didReceiveData:(NSData *)data
-{
-    [rawXmlData appendData:data];
-}
--(void) connection:(NSURLConnection *)connection 
-  didFailWithError:(NSError *)error
-{
-    [ms appendFormat:@"%@\n", [error localizedDescription]];
-    textView.text = ms;
-}
-
--(void) connectionDidFinishLoading:(NSURLConnection *)connection
-{
-    NSString *xml = [[NSString alloc] 
-                     initWithData:rawXmlData encoding:NSUTF8StringEncoding];
-    [ms appendFormat:@"%@\n", xml];
-    textView.text = ms;
-    NSXMLParser *parser = 
-    [[NSXMLParser alloc] initWithData:rawXmlData];
-    [parser setDelegate:self];
-    //WARNING! The parsing is synchronous!
-    [parser parse];
-}
--(void) parserDidStartDocument:(NSXMLParser *)parser
-{
-    [ms appendString:@"Start XML Parsing!\n"];
-}
--(void) parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary *)attributeDict
-{
-    [ms appendFormat:@"%@\n", elementName];
-}
--(void) parserDidEndDocument:(NSXMLParser *)parser
-{
-    [ms appendString:@"End XML Parsing!\n"];
-    textView.text = ms;
+    // implementation variables
+    NSMutableArray *myVideos;
+    UITextView *myTextView;
+    Video *video;
+    enum ParserState state;
 }
 
 @synthesize window = _window;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
     // Override point for customization after application launch.
+    // get the application frame and add a textView to it
+    CGRect rect = [[UIScreen mainScreen] applicationFrame];
+    myTextView = [[UITextView alloc] initWithFrame:rect];
+    myTextView.editable = NO;
+    myTextView.text = @"Loading...";
+    [self.window addSubview:myTextView];
+    
+    [self performSelectorInBackground:@selector(doXMLParsing:) 
+                           withObject:nil];
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
     return YES;
 }
-							
+
+// the XML parser, started in it's own thread
+-(void)doXMLParsing:(id)sender
+{
+    NSString *urlString = @"http://stream.xaoc.org:7767/simple.xsl";
+
+    // create a URL object
+    NSURL *url = [NSURL URLWithString:urlString];
+    // create a parser that reads from the URL object; this blocks, which 
+    // is why it's in it's own thread
+    NSXMLParser *parser = [[NSXMLParser alloc] initWithContentsOfURL:url];
+    // set the delegate class to this (self) class
+    [parser setDelegate:self];
+    // blocking call
+    [parser parse];
+}
+
+// parsing error occured; notified the GUI running in the main thread
+-(void)parser:(NSXMLParser *)parser parseErrorOccurred:(NSError *)parseError
+{
+    [self performSelectorOnMainThread:@selector(displayErrorMsg:) 
+                            // localize the error description
+                           withObject:[parseError localizedDescription]
+                        waitUntilDone:NO];
+}
+
+// notify when the parser started
+-(void)parserDidStartDocument:(NSXMLParser *)parser
+{
+    myVideos = [[NSMutableArray alloc] init];
+}
+
+// we're starting to parse an <element>
+-(void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName 
+ namespaceURI:(NSString *)namespaceURI 
+qualifiedName:(NSString *)qName 
+   attributes:(NSDictionary *)attributeDict
+{
+    // do we have a 'video' element
+    if ( [elementName isEqualToString:@"video"] ) {
+        video = [[Video alloc] init];
+        state = xmlInit;
+        return;
+    }
+    if ( [elementName isEqualToString:@"title"] ) {
+
+        state = xmlTitle;
+        return;
+    }
+    if ( [elementName isEqualToString:@"description"] ) {
+        video.desc = @"";
+        state = xmlDescription;
+        return;
+    }
+}
+
+// we're starting to parse characters in an <element>
+-(void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string
+{
+    // use the current state to decide what to do
+    switch (state) {
+        case xmlTitle:
+            video.title = string;
+            state = xmlInit;
+            //break;
+            return;
+        case xmlDescription:
+            video.desc = string;
+            state = xmlInit;
+            [myVideos addObject:video];
+            //break;
+            return;
+        default:
+            //break;
+            return;
+    }
+}
+                    
+// notify when the document is finished parsing
+-(void)parserDidEndDocument:(NSXMLParser *)parser
+{
+    [self performSelectorOnMainThread:@selector(updateGUI:) 
+                           withObject:nil 
+                        waitUntilDone:NO];
+}
+
+// update the GUI; display the videos by calling the 'description' message
+-(void) displayErrorMsg:(id)object
+{
+    myTextView.text = [object description];
+}
+
+// update the GUI; display the videos by calling the 'description' message
+-(void) updateGUI:(id)sender
+{
+    myTextView.text = [myVideos description];
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     /*
