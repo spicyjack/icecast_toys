51505286301dab96639ad5a12157bd54cc4072d8
Brian Manning <brian@xaoc.org>
2012-03-27 23:23:32 -0700

    Added stubs for icecast stream class

---
+++ ios/TestCode/IcecastStatus/IcecastStatus/IcecastStream.h
@@ -0,0 +1,13 @@
+//
+//  IcecastStream.h
+//  IcecastStatus
+//
+//  Created by Brian Manning on 3/27/12.
+//  Copyright (c) 2012 Example Company Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IcecastStream : NSObject
+
+@end
---
+++ ios/TestCode/IcecastStatus/IcecastStatus/IcecastStream.m
@@ -0,0 +1,13 @@
+//
+//  IcecastStream.m
+//  IcecastStatus
+//
+//  Created by Brian Manning on 3/27/12.
+//  Copyright (c) 2012 Example Company Inc. All rights reserved.
+//
+
+#import "IcecastStream.h"
+
+@implementation IcecastStream
+
+@end
