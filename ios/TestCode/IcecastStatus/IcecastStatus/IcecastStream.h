eb7f33ac9efcb0de93ad7f0f83206fcaa677075e
Brian Manning <brian@xaoc.org>
2012-03-29 23:53:39 -0700

    added properties for an icecast stream object
    - added doxygen docs

--- ios/TestCode/IcecastStatus/IcecastStatus/IcecastStream.h
+++ ios/TestCode/IcecastStatus/IcecastStatus/IcecastStream.h
@@ -1,13 +1,27 @@
 //
 //  IcecastStream.h
-//  IcecastStatus
+//  \brief IcecastStream, an individual stream running on an IcecastServer
 //
-//  Created by Brian Manning on 3/27/12.
-//  Copyright (c) 2012 Brian Manning. All rights reserved.
+//  \author Brian Manning 
+//  \date Created on 3/27/12.
+//  \copyright Copyright (c) 2012 Brian Manning. All rights reserved.
+
 //
 
 #import <Foundation/Foundation.h>
 
 @interface IcecastStream : NSObject
+
+// attributes
+@property (nonatomic, strong) NSString *mountPoint;
+@property (nonatomic, strong) NSString *streamName;
+@property (nonatomic, strong) NSDate *streamStart;
+@property (nonatomic) int currentListeners;
+@property (nonatomic, strong) NSString *streamDescription;
+@property (nonatomic, strong) NSString *currentlyPlayingSong;
+@property (nonatomic, strong) NSURL *streamURL;
+
+// methods
+-(NSString *) description;
 
 @end
